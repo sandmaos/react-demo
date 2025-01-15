565298c71ae0ae3265dd1195f32d65f6fa50dc8d
Your Name <you@example.com>
2025-01-14 16:09:58 -0800

    25-01-14

---
+++ src_pdf/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ThemeProvider theme={createTheme({ palette: { mode: 'dark' } })}>
+        <CssBaseline />
+        <App />
+    </ThemeProvider>
+)
